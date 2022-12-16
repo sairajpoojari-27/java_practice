e1e69b0c37cd4acffb333caf16a4c2787845a451
sairaj_poojari <sairajpoojary5@gmail.com>
2022-12-16 18:42:35 +0530

    added multiply.java

---
+++ Multiply.java
@@ -0,0 +1,14 @@
+
+class Multiply
+{
+    public static void main(String[] args) 
+    {
+       int a=10;
+       int b=20;
+       int sum=a+b;
+       System.out.println("sum od "+a+"and "+b+"is"+sum); 
+    }
+}
+    
+
+
